20bd1a8cfd7846a814874f8424cce15f1474f6dd
krattya <146937259+krattya@users.noreply.github.com>
2025-04-10 13:08:03 +0300

    saving response and question to db

--- chatbot.py
+++ chatbot.py
@@ -1,19 +1,40 @@
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+from pymongo import MongoClient
 
 
 load_dotenv()
+
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client["Verkkokauppa"]
+collection = db["Chatbot"]
+
 openai_api_key = os.getenv("OPENAI_KEY")
 openai = OpenAI(api_key=openai_api_key)
 
+def get_faq_context():
+    faqs = collection.find({"answer": {"$exists": True}})
+    return "\n".join(f"{faq['question']}: {faq['answer']}" for faq in faqs)
+
 def chat_with_gpt(user_input):
+
+    collection.insert_one({
+        "question": user_input
+    })
+
+    context = get_faq_context()
+    conversation = [
+        {"role": "system", "content": "You are a helpful customer service bot who answers politely to customers' questions."},
+        {"role": "system", "content": f"Here are some FAQs:\n{context}"},
+        {"role": "user", "content": user_input}
+    ]
+
     response = openai.chat.completions.create(
         model="gpt-4o-mini",
-        messages=[
-            {"role": "system", "content": "You are a helpful customer service bot who answers politely to customers' questions."},
-            {"role": "user", "content": user_input}
-        ]
+        messages=conversation
+            
     )
+    
     return response.choices[0].message.content.strip()
 
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from chatbot import chat_with_gpt
 if __name__ == "__main__":
-    from chatbot import chat_with_gpt
+    print("Chatbot is ready! type in quit, if you want to exit")
     
     while True:
         user_input = input("You: ")
-        if user_input.lower() in ["quit", "exit", "bye"]:
+        if user_input.lower() in ["quit"]:
             break
 
         response = chat_with_gpt(user_input)
