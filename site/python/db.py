30ad7aacb550715bfec0f613c84c3ee762d20a49
Tuukka Juutilainen <80672631+Farmapu@users.noreply.github.com>
2025-04-13 22:43:17 +0300

    Valmiiks kuntoon filuje määritys
    Kärsin pt2

--- site/python/db.py
+++ site/python/db.py
@@ -5,4 +5,9 @@ from test import mongodb
 
 client = MongoClient(os.getenv(mongodb['str']))
 cluster = client['Cluster0']
-user = cluster['']
+cpu = cluster['Cpu']
+customerInfo = cluster['CustomerInfo']
+gpu = cluster['Gpu']
+mobo = cluster['Mobo']
+orders = cluster['Orders']
+ram = cluster['Ram']
